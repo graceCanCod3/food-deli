415f40ef61e16dd2510d0d2c87fc7425da158109
graceCanCod3 <gmadikaegbu@gmail.com>
2024-11-29 16:45:39 -0500

    Add/Remove from cart functionality

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id,name,price,description,image}) => {
-  //tester
+    
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+  
   return (
     <div className='food-item'>
-        <div className="food-item-container">
+        <div className="food-item-img-container">
             <img className='food-item-image' src={image} alt="" />
+            {!cartItems[id]
+                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
+                :<div className='food-item-counter'>
+                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                    <p>{cartItems[id]}</p>
+                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+                </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
@@ -15,6 +26,7 @@ const FoodItem = ({id,name,price,description,image}) => {
                 <img src={assets.rating_starts} alt="" />
             </div>
             <p className="food-item-desc">{description}</p>
+            <p className="food-item-price">${price}</p>
         </div>
     </div>
   )
